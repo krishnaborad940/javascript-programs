8174e6824eef9a3a84787191210f8ac635ff58eb
krishnaborad940 <boradkrishna940.com>
2025-04-29 15:42:47 +0530

    grand program

---
+++ marks-grande.js
@@ -0,0 +1,13 @@
+let marks=prompt ("enter your marks:-")
+
+if(marks>=80 && marks<=100){
+    console.log("A")
+}else if( marks>=70 && marks<=89){
+    console.log("b")
+}else if( marks>=60 && marks<=69){
+    console.log("C")
+}else if( marks>=50 && marks<=59){
+    console.log("d")
+}else if( marks>=0 && marks<=49){
+    console.log("f")
+}
