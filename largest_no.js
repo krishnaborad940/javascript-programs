d05b64ed93335bde73b8a02b355aafbebb159fc8
krishnaborad940 <boradkrishna940.com>
2025-04-29 17:28:42 +0530

    5 no to find largest no

---
+++ largest_no.js
@@ -0,0 +1,17 @@
+let a=prompt("enter a value:-");
+let b=prompt("enter b value:-");
+let c=prompt("enter c value:-");
+let d=prompt("enter d value:-");
+let e=prompt("enter e value:-");
+
+if(a>b && a>c && a>d && a>e){
+     console.log(a)
+}else if(b>a && b>c && b>d && b>e){
+    console.log(b)
+}else if(c>a && c>b && c>d && c >e){
+    console.log(c)
+}else if(d>a && d>b && d>c && d>e){
+    console.log(d)
+}else{
+    console.log(e)
+}
